98aa475accd1255821b9c23192efd9333b737b3e
Diogo de Azevedo Silva <diogodeazevedosilva@gmail.com>
2021-04-03 19:46:32 -0300

    fix: update port

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import http from 'http';
 
+const PORT = process.env.PORT ?? 5678;
+
 const server = http.createServer((_, res) => {
   res.writeHead(200);
   res.end('Hello world!');
 });
 
-server.listen(5678);
+server.listen(PORT);
